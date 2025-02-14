1360bfd9fa7360cad43fea6df92fef639596973c
Mark Peters <mapeters101@earthlink.net>
2025-02-14 15:56:23 -0500

    No code for lesson003.  Course Slides for course.

---
+++ lesson004.py
@@ -0,0 +1,7 @@
+"""
+Course Slides
+Each video lecture in this course has the corresponding slides attached as a resource. However, you can also just download them all as a single PDF here.
+
+Resources for this lecture
+Python Deep Dive 1.pdf
+"""
